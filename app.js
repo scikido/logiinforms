8f994393d0b0d2f39767301504ed62332be61271
Tanmay <tanmay.navgire24@gmail.com>
2021-10-07 00:11:09 +0530

    new data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 80
+const port = process.env.PORT || 80
 // const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 
