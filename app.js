3fd54e7971272e35cf576fa8288ee5d336a226d4
Tanmay <tanmay.navgire24@gmail.com>
2021-10-07 23:53:35 +0530

    2nd

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bcrypt = require('bcryptjs')
 const port = process.env.PORT || 80
 // const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 
+const DB = process.env.DATABASE
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => { console.log("success") }).catch((err) => console.log("fail"))
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    password: String
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
 
-});
+    },
+    work: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cpassword: {
+        type: String,
+        required: true
+    }
 
-const User = mongoose.model('User', userSchema);
+})
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12)
+        this.cpassword = await bcrypt.hash(this.cpassword, 12)
+    }
+    next();
+})
+const User = mongoose.model('USER', userSchema)
+
 
 app.use('/static', express.static('static'))
 app.use(express.urlencoded())
